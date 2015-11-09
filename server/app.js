5efabadb4c76143e1a8d18272ca3c8fad800e926
Leonel Machava <leonelmachava@gmail.com>
2015-11-09 13:53:28 +0200

    Add Mongoose connection in the beginning of server workflow

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
 var config = require('./config'), 
-	express = require('express');
+	express = require('express'),
+	mongoose = require('mongoose');
 
-var app = express();
-
-app.get('/', function rootRoute(req, res) {
-	res.send('TopTal Leonel Machava Project');
+mongoose.connect(config.DATABASE_URL, function(err) {
+	if (err) {
+		throw err;
+	}
+	
+	console.log('Connected to database.');
+	
+	var app = express();
+	
+	app.get('/', function rootRoute(req, res) {
+		res.send('TopTal Leonel Machava Project');
+	});
+	
+	app.listen(config.PORT);
+	console.log('Listening on port ' + config.PORT);	
 });
-
-app.listen(config.PORT);
-console.log('Listening on port ' + config.PORT);
