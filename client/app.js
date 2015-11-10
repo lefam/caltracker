3bf427535d71e66898e213deeea8722b20d729f6
Leonel Machava <leonelmachava@gmail.com>
2015-11-10 20:44:58 +0200

    Improve gulp build task and add some front end client states
    Improved gulp build task to copy partials and angular ui-router module.
    Added basic client states using ui-router module

--- client/app.js
+++ client/app.js
@@ -1,3 +1,19 @@
 (function() {
-    angular.module('app');
-});
+    angular
+        .module('app', ['ui.router'])
+        .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state('login', {
+                    url: '/login',
+                    templateUrl: 'partials/login.html'
+                })
+                .state('signup', {
+                    url: '/signup',
+                    templateUrl: 'partials/signup.html'
+                })
+                .state('home', {
+                    url: '/home',
+                    templateUrl: 'partials/home.html'
+                });
+        }])
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,15 @@ gulp.task('vendor', function vendorTask() {
 
 	gulp.src(['client/vendor/angular/angular.min.js', 'client/vendor/angular/angular.min.js.gzip'])
 		.pipe(gulp.dest('public/vendor/angular/'));
+
+    gulp.src(['client/vendor/angular-ui-router/release/angular-ui-router.min.js'])
+        .pipe(gulp.dest('public/vendor/angular-ui-router/'));
 });
 
 gulp.task('client', function clientTask() {
+    gulp.src('client/partials/**/*')
+        .pipe(gulp.dest('public/partials/'));
+
     gulp.src('client/css/**/*')
         .pipe(gulp.dest('public/css/'));
 
