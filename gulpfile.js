9a0a46d3171ee133d4189d7ab4fc3617bb0298ca
Leonel Machava <leonelmachava@gmail.com>
2015-11-20 15:55:02 +0200

    Improve gulp build tasks adding cleaning of files, css and js minification (on production)

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,20 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    del = require('del'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css'),
+    removeCode = require('gulp-remove-code');
+
+var clientJS = [
+    'client/app.js',
+    'client/directives/**/*',
+    'client/services/**/*',
+    'client/controllers/**/*'
+];
+
+gulp.task('clean', function() {
+    return del.sync(['public/']);
+});
 
 gulp.task('vendor', function vendorTask() {
 	gulp.src('client/vendor/bootstrap/dist/**/*')
@@ -18,11 +34,11 @@ gulp.task('vendor', function vendorTask() {
 });
 
 gulp.task('client', function clientTask() {
+    gulp.src('client/app.js')
+        .pipe(gulp.dest('public/'));
+
     gulp.src('client/partials/**/*')
         .pipe(gulp.dest('public/partials/'));
-
-    gulp.src('client/css/**/*')
-        .pipe(gulp.dest('public/css/'));
 
     gulp.src('client/directives/**/*')
         .pipe(gulp.dest('public/directives/'));
@@ -33,17 +49,35 @@ gulp.task('client', function clientTask() {
     gulp.src('client/controllers/**/*')
         .pipe(gulp.dest('public/controllers/'));
 
-    gulp.src('client/directives/**/*')
-        .pipe(gulp.dest('public/directives/'));
+    gulp.src('client/css/**/*')
+        .pipe(gulp.dest('public/css/'));
 
     gulp.src('client/index.html')
+        .pipe(removeCode({development: true}))
         .pipe(gulp.dest('public/'));
 
-    gulp.src(['client/app.js'])
+});
+
+gulp.task('client-dist', function clientDistTask() {
+    gulp.src(clientJS )
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/'));
+
+    gulp.src('client/css/**/*')
+        .pipe(minifyCss())
+        .pipe(gulp.dest('public/css/'));
+
+    gulp.src('client/partials/**/*')
+        .pipe(gulp.dest('public/partials/'));
+
+    gulp.src('client/index.html')
+        .pipe(removeCode({production: true}))
         .pipe(gulp.dest('public/'));
 });
 
-gulp.task('build', ['vendor', 'client']);
+gulp.task('build-dev', ['clean', 'vendor', 'client']);
+gulp.task('build', ['clean', 'vendor', 'client-dist']);
 
 gulp.task('watch', function watchTask() {
    gulp.watch('client/**', ['build']);
